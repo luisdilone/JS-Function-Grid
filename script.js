fa1103a1c794bb230c2fc10fc529c8abbc83f0f1
Luis Angel Dilone <ldilone@aclanyc.com>
2017-02-27 13:20:15 +0000

    Make Corners, Smiley Face, and S

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //Write your function calls below this line
+$(document).ready(function(){
+setColor(0,0,"black");
+setColor(9,9,"black");
+setColor(9,0,"black");
+setColor(0,9,"black");
+//Corners
+
+setColor(2,1,"red");
+setColor(2,3,"red");
+setColor(4,1,"red");
+setColor(5,1,"red");
+setColor(5,2,"red");
+setColor(5,3,"red");
+setColor(4,3,"red");
+//Happy Face
+
+setColor(2,7,"blue");
+setColor(2,6,"blue");
+setColor(3,6,"blue");
+setColor(4,7,"blue");
+setColor(5,7,"blue");
+setColor(5,6,"blue");
+// S
+
+});
